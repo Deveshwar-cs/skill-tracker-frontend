8f4e1e8207eab771197c74681d93faf7feef9084
Deveshwar-cs <goboby744@gmail.com>
2026-01-27 11:34:38 +0530

    Adding base url of backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://skill-tracker-backend-zwzc.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
